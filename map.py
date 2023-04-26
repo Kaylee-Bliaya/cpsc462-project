3571f03748c0a68ccc35b7c918cfaaecb4f9a4cf
Kbliaya <kbliaya@csu.fullerton.edu>
2023-04-25 23:30:26 -0700

    added comments to map.py

--- map.py
+++ map.py
@@ -2,12 +2,14 @@ import pygame
 from sys import exit
 
 class Button():
+    # method to initialize a button object
     def __init__(self, x, y, img):
         self.img = img
         self.rect = self.img.get_rect()
         self.rect.topleft = (x, y)
         self.clicked = False
 
+    # method to draw a button object and make it clickable for users
     def draw(self):
         action = False
         mouse_pos = pygame.mouse.get_pos()
@@ -23,7 +25,8 @@ class Button():
 
         return action
 
-def loadAndScaleBtn(imgName, type):
+# function to load an image and scale it to fit the game screen
+def loadAndScaleImg(imgName, type):
     if type == 'map':
         imgScale = 3
     elif type == 'btn':
@@ -32,16 +35,21 @@ def loadAndScaleBtn(imgName, type):
         imgScale = 5
 
     img = pygame.image.load(imgName)
+
     imgWidth = img.get_rect().width
     imgHeight = img.get_rect().height
     img = pygame.transform.scale(img, (imgWidth/imgScale, imgHeight/imgScale))
+
     return img
 
+# function to check if the previous level has been passed
 def checkIfLevelPassed(currLevel, levelsPassed):
     if currLevel - 1 in levelsPassed:
         return True
     return False
 
+# function to add the passed level to a list called levelsPassed
+# if the level has been passed previously, don't add it to the list
 def addLevel(currLevel, levelsPassed):
     if currLevel not in levelsPassed:
         levelsPassed.append(currLevel)
@@ -49,6 +57,7 @@ def addLevel(currLevel, levelsPassed):
 
 pygame.init()
 
+# set the screen width and height
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
 
@@ -57,21 +66,21 @@ pygame.display.set_caption('Path of Conquest')
 clock = pygame.time.Clock()
 
 # load and scale background map image
-image = loadAndScaleBtn('map_graphics/map.png', 'map')
+map_img = loadAndScaleImg('map_graphics/map.png', 'map')
 
 # load and scale button images
-btn1_img = loadAndScaleBtn('map_graphics/btn1.png', 'btn')
-btn2_img = loadAndScaleBtn('map_graphics/btn2.png', 'btn')
-btn3_img = loadAndScaleBtn('map_graphics/btn3.png', 'btn')
-btn4_img = loadAndScaleBtn('map_graphics/btn4.png', 'btn')
-btn5_img = loadAndScaleBtn('map_graphics/btn5.png', 'btn')
-btn6_img = loadAndScaleBtn('map_graphics/btn6.png', 'btn')
-btn7_img = loadAndScaleBtn('map_graphics/btn7.png', 'btn')
-btn8_img = loadAndScaleBtn('map_graphics/btn8.png', 'btn')
-btn9_img = loadAndScaleBtn('map_graphics/btn9.png', 'btn')
-btn10_img = loadAndScaleBtn('map_graphics/btn10.png', 'btn')
-menuBtn_img = loadAndScaleBtn('map_graphics/menu.png', 'menu')
-inventoryBtn_img = loadAndScaleBtn('map_graphics/inventory.png', 'inventory')
+btn1_img = loadAndScaleImg('map_graphics/btn1.png', 'btn')
+btn2_img = loadAndScaleImg('map_graphics/btn2.png', 'btn')
+btn3_img = loadAndScaleImg('map_graphics/btn3.png', 'btn')
+btn4_img = loadAndScaleImg('map_graphics/btn4.png', 'btn')
+btn5_img = loadAndScaleImg('map_graphics/btn5.png', 'btn')
+btn6_img = loadAndScaleImg('map_graphics/btn6.png', 'btn')
+btn7_img = loadAndScaleImg('map_graphics/btn7.png', 'btn')
+btn8_img = loadAndScaleImg('map_graphics/btn8.png', 'btn')
+btn9_img = loadAndScaleImg('map_graphics/btn9.png', 'btn')
+btn10_img = loadAndScaleImg('map_graphics/btn10.png', 'btn')
+menuBtn_img = loadAndScaleImg('map_graphics/menu.png', 'menu')
+inventoryBtn_img = loadAndScaleImg('map_graphics/inventory.png', 'inventory')
 
 # create button instances
 btn1 = Button(35, 300, btn1_img)
@@ -87,22 +96,30 @@ btn10 = Button(1135, 102, btn10_img)
 menuBtn = Button(0, 0, menuBtn_img)
 inventoryBtn = Button(50, 0, inventoryBtn_img)
 
+# list to record what levels the player has passed
 levelsPassed = []
 
 run = True
 while run:
+    # if the player selects the exit button, quit the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.blit(image, (0, -200))
+    # display the map image on the game screen
+    screen.blit(map_img, (0, -200))
 
+    # draw the menu and inventory buttons on the game screen
     if menuBtn.draw():
         print('menu')
     if inventoryBtn.draw():
         print('inventory')
 
+    # draw the level buttons
+    #   * if the level button is drawn and the previous level has been passed
+    #       allow the player to select the next level button
+    #   * when a level has been passed add the level to the list levelsPassed
     if btn1.draw():
         print('level 1')
         addLevel(1, levelsPassed)
